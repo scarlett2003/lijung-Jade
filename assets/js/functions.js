6d98c4d69f8d69201c3e927888a5d42683d09f47
vialley-imac <jovilee09@gmail.com>
2015-09-05 02:17:11 +0800

    fix scrollTop.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,35 +1,39 @@
-$( document ).ready(function() {
+$(function () {
 
-  	$('.backTop').each(function(){
+    /*
+     * Back-toTop button (Smooth scroll)
+     */
+    $('.backTop').each(function () {
 
-  		var el = scrollabelElement('html', 'body');
+        
+        // 檢測html、body何者為可捲動元素
+        var $el = $(scrollableElement('html', 'body'));
 
-  		$(this).on('click', function(event){
-  			event.preventDefault();
-  			$(el).animate({ scrollTop: 0 }, 250);
-  		});
+        // 設定按鈕的點擊事件
+        $(this).on('click', function (event) {
+            event.preventDefault();
+            $el.animate({ scrollTop: 0 }, 250);
+        });
+    });
 
-  	});
 
-  	function scrollabelElement(){
-
-  		var i, len, el, $el, scrollabel;
-  		for (i = 0, len = arguments.length; i < len; i++) {
-  			el = arguments[i],
-  			$el = $(el);
-
-  			if ($el.scrollTop() > 0){
-  				return el;
-  			} else {
-  				$el.scrollTop(1);
-  				scrollable = $el.scrollTop() > 0;
-  				$el.scrollTop(0);
-  				if (scrollabel) {
-  					return el;
-  				}
-  			}
-  		}
-  		return [];
-  	}
+    function scrollableElement (elements) {
+        var i, len, el, $el, scrollable;
+        for (i = 0, len = arguments.length; i < len; i++) {
+            el = arguments[i],
+            $el = $(el);
+            if ($el.scrollTop() > 0) {
+                return el;
+            } else {
+                $el.scrollTop(1);
+                scrollable = $el.scrollTop() > 0;
+                $el.scrollTop(0);
+                if (scrollable) {
+                    return el;
+                }
+            }
+        }
+        return [];
+    }
 
 });
